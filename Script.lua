ccf594014add57feda921703e1be8aa571eead42
Zlv <141329634+ZlvXs@users.noreply.github.com>
2024-06-25 12:20:44 -0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -31,6 +31,61 @@ HomeTab:TextBox("Hitbox Transparency", function(number)
 	getgenv().HitboxTransparency = number
 end)
 
+HomeTab:Section("Features")
+
+HomeTab:Button("Hitlock", function()
+    plr:GetMouse().KeyDown:Connect(function(key)
+        if key == 'r' then
+            tar = nil
+            for _,v in next,workspace:GetDescendants() do
+                if v.Name == 'SelectedPlayer' then
+                    v:Destroy()
+                end
+            end
+            local n_plr, dist
+            for _, v in pairs(game.Players:GetPlayers()) do
+                if v ~= plr and plr.Character and plr.Character:FindFirstChild'HumanoidRootPart' then
+                    local distance = v:DistanceFromCharacter(plr.Character.HumanoidRootPart.Position)
+                    if v.Character and (not dist or distance <= dist) and v.Character:FindFirstChildOfClass'Humanoid' and v.Character:FindFirstChildOfClass'Humanoid'.Health>0 and v.Character:FindFirstChild'HumanoidRootPart' then
+                        dist = distance
+                        n_plr = v
+                    end
+                end
+            end
+            local sp = Instance.new('SelectionBox',n_plr.Character.Torso)
+            sp.Name = 'SelectedPlayer'
+            sp.Adornee = n_plr.Character.HumanoidRootPart
+            sp.Color3 = Color3.new(255,255,255)
+            sp.Transparency = 0.25
+            tar = n_plr
+        elseif key == 'q' and tar and plr.Character then
+            for _,v in next,plr.Character:GetDescendants() do
+                if v:IsA'Tool' and v.Name ~= 'Kawaii Revolver' and not v:FindFirstChild'ClientCD' and v:FindFirstChild'DamageRemote' and v:FindFirstChild'Cooldown' and tar and tar.Character and tar.Character:FindFirstChildOfClass'Humanoid' then
+                    AddCd(v,v.Cooldown.Value)
+                    if v:FindFirstChild'Attack' and plr.Character:FindFirstChildOfClass'Humanoid' then
+                        plr.Character:FindFirstChildOfClass'Humanoid':LoadAnimation(v.Attack):Play()
+                    end
+                    if v:FindFirstChild'Blade' then
+                        for _,x in next,v.Blade:GetChildren() do
+                            if x:IsA'Sound' then
+                                x:Play()
+                            end
+                        end
+                    end
+                    v.DamageRemote:FireServer(tar.Character:FindFirstChildOfClass'Humanoid')
+                elseif v:IsA'Tool' and v.Name == 'Kawaii Revolver' and not v:FindFirstChild'ClientCD' and v:FindFirstChild'ReplicateRemote' and v:FindFirstChild'Barrel' and v.Barrel:FindFirstChild'Attachment' and tar and tar.Character and tar.Character:FindFirstChild'Torso' then
+                    v.Parent = plr.Character
+                    AddCd(v,1)
+                    rs.Stepped:wait()
+                    plr.Character:FindFirstChildOfClass'Humanoid':LoadAnimation(v.Fire):Play()
+                    Shoot(v.Barrel.Attachment.WorldPosition,tar.Character:FindFirstChild("Torso").Position,v)
+                    v.ReplicateRemote:FireServer(tar.Character:FindFirstChild("Torso").Position)            
+                end
+            end
+        end
+end)
+end)
+
 HomeTab:Button("Sword", function()
 	if game.Players.LocalPlayer.Character:FindFirstChild("Loaded") then
 		game.Players.LocalPlayer.Character.Loaded:Destroy()
